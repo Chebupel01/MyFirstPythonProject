0d53b0dfab1cf30096567996fe85c8e011472203
chebupel01 <mironov.igor120.igor@gmail.com>
2023-10-30 16:14:36 +0300

    Добавил импорт

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import io
+import time
+from datetime import datetime
+from PyQt5.QtGui import QPixmap, QTransform, QColor, QImage
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QLabel, QWidget
+from PyQt5.QtCore import QPoint
+from res_rc import *
+from res_rc1 import *
+from res_rc2 import *
+from res_rc3 import *
+from res_rc4 import *
+import sqlite3
